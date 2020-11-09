cdd672505d351ef399d578ba25b4eba55783b2ea
Samuel-dev-pixel <70909772+Samuel-dev-pixel@users.noreply.github.com>
2020-11-08 22:45:08 -0500

    Add files via upload

---
+++ JS-Personajes-animados.js
@@ -0,0 +1,74 @@
+function Cambio1_Amapola() {
+	var amapola = document.getElementById("Amimg");
+	amapola.src = "Animación-de-Amapola-3.gif"
+}
+function Cambio2_Amapola() {
+	var amapola = document.getElementById("Amimg");
+	amapola.src = "Animación-de-Amapola-2.gif"
+}
+function Cambiop_Amapola() {
+	var amapola = document.getElementById("Amimg");
+	amapola.src = "Imagen-de-Amapola.gif"
+}
+
+function Cambio1_Zlatan() {
+	var zlatan = document.getElementById("Zlimg");
+	zlatan.src = "Animación-de-Zlatan-3.gif"
+}
+function Cambio2_Zlatan() {
+	var zlatan = document.getElementById("Zlimg");
+	zlatan.src = "Animación-de-Zlatan-2.gif"
+}
+function Cambiop_Zlatan() {
+	var zlatan = document.getElementById("Zlimg");
+	zlatan.src = "Imagen-de-Zlatan.gif"
+}
+
+function Cambio1_Jara() {
+	var jara = document.getElementById("Jaimg");
+	jara.src = "Jara-sencillo.gif"
+}
+function Cambio2_Jara() {
+	var jara = document.getElementById("Jaimg");
+	jara.src = "Jara-complejo.gif"
+}
+function Cambiop_Jara() {
+	var amapola = document.getElementById("Jaimg");
+	amapola.src = "Jara.png"
+}
+function Cambio1_Milovan() {
+	var milovan = document.getElementById("Miimg");
+	milovan.src = "Milovan-sencillo.gif"
+}
+function Cambio2_Milovan() {
+	var milovan = document.getElementById("Miimg");
+milovan.src = "Milovan-complejo.gif"
+}
+function Cambiop_Milovan() {
+	var milovan = document.getElementById("Miimg");
+	milovan.src = "Milovan.png"
+}
+function Cambio1_Theo() {
+	var theo = document.getElementById("Thimg");
+	theo.src = "Theo-complejo.gif"
+}
+function Cambio2_Theo() {
+	var theo = document.getElementById("Thimg");
+	theo.src = "Theo-sencillo.gif"
+}
+function Cambiop_Theo() {
+	var theo = document.getElementById("Thimg");
+	theo.src = "Theo.png"
+}
+function Cambio1_Robb() {
+	var robb = document.getElementById("Roimg");
+	robb.src = "robb-sencillo.gif"
+}
+function Cambio2_Robb() {
+	var robb = document.getElementById("Roimg");
+	robb.src = "robb-complejo.gif"
+}
+function Cambiop_Robb() {
+	var robb = document.getElementById("Roimg");
+	robb.src = "Robb.png"
+}
